b4f502fb568dea871101a18c73660ecdafbd3eb0
svikramjeet <vikramjeet@ucreate.co.in>
2019-09-22 10:05:53 +0530

    Added composer entrypoint

--- entrypoint.php
+++ entrypoint.php
@@ -1,2 +1,22 @@
 <?php
-echo "Hello World!!!";
+include_once('vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+
+$endpoint = getenv('WEBHOOK_URL');
+$payload  = [
+    'text' => getenv('TEXT') ?? "Hello",
+    'channel' => getenv('CHANNEL') ?? "general",
+    'username' => getenv('USERNAME') ?? "svikramjeet",
+    'icon_url' => getenv('ICON') ?? ':ghost:'
+];
+
+$encoded = json_encode($payload, JSON_UNESCAPED_UNICODE);
+
+    if ($encoded === false) {
+        throw new RuntimeException(sprintf('JSON encoding error %s: %s', json_last_error(), json_last_error_msg()));
+    }
+
+$guzzle = new GuzzleHttp\Client();
+$guzzle->post($endpoint, ['body' => $encoded]);
